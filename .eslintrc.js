c0d3636b2904b1aee8448a9ed81067898f40c405
MatheusShirakawa <matheus_shirakawa@outlook.com>
2023-11-04 13:49:34 -0300

    :tada: first commit

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+	parser:'@typescript-eslint/parser',
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	plugins:['@typescript-eslint'],
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier/@typescript-eslint',
+		'standard'
+	],
+	'overrides': [
+		{
+			'env': {
+				'node': true
+			},
+			'files': [
+				'.eslintrc.{js,cjs}'
+			],
+			'parserOptions': {
+				'sourceType': 'script'
+			}
+		}
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
